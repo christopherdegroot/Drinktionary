6173a2db92dc0e7376999d607911e2c4a18a1ead
Chris De Groot <christopher.neil.degroot@gmail.com>
2022-12-01 01:03:43 -0800

    successfully fetch data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
-const Index = () => <p> hello world</p>;
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Index = () => {
+  const [data, setData] = useState(null);
+
+  const getRandomCocktail = async () => {
+    try {
+      axios
+        .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        .then((res) => {
+          console.log("res.data:", res.data);
+          setData(res.data);
+        });
+    } catch (error) {
+      console.log("error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getRandomCocktail();
+  }, []);
+
+  return <p> hello world</p>;
+};
 
 export default Index;
