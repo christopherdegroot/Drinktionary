91be619704b043e2cb2d16de716606c059562fb5
Chris De Groot <christopher.neil.degroot@gmail.com>
2022-12-01 01:44:11 -0800

    add more options, ensure options are using correct searchTerm for api

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,11 @@ const Index = () => {
   // function to get 1 random cocktal
   const getData = async (searchTerm?) => {
     axios
-      .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+      .get(
+        searchTerm
+          ? `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${searchTerm}`
+          : "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+      )
       .then((res) => {
         console.log("res.data:", res.data);
         setData(res.data.drinks[0]);
@@ -19,6 +23,10 @@ const Index = () => {
         console.log("error:", error);
         setLoading;
       });
+  };
+
+  const handleChange = function (value?) {
+    console.log("value", value);
   };
 
   // Get random cocktail on page load
@@ -40,13 +48,19 @@ const Index = () => {
       <p>{data.strCategory}</p>
       <p>{data.strGlass}</p>
       <button onClick={() => getData()}>New Cocktail</button>
-      <select name="drinks" id="drinks">
+      <select
+        onChange={(e) => getData(e.target.value)}
+        name="drinks"
+        id="drinks"
+      >
         <option value="cocktail">Cocktail</option>
-        <option value="ordinaryDrink">Ordinary Drink</option>
+        <option value="ordinary_drink">Ordinary Drink</option>
         <option value="shot">Shot</option>
-        <option value="punchParty">Punch/Party Drink</option>
+        <option value="punch_/_party_drink">Punch/Party Drink</option>
         <option value="beer">Beer</option>
-        <option value="coffeeTea">Coffee/Tea</option>
+        <option value="shake">Shake</option>
+        <option value="coffee_/_tea">Coffee/Tea</option>
+        <option value="other/unknown">Other/Unknown</option>
       </select>
     </>
   );
