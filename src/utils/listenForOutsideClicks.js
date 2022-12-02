00c825c85a0c37d4ef49a509ab86079c3624d4b1
Chris De Groot <christopher.neil.degroot@gmail.com>
2022-12-01 20:55:40 -0800

    add console log for debugging

--- src/utils/listenForOutsideClicks.js
+++ src/utils/listenForOutsideClicks.js
@@ -10,6 +10,7 @@ export default function listenForOutsideClicks(
     setListening(true);
     [`click`, `touchstart`].forEach((type) => {
       document.addEventListener(`click`, (evt) => {
+        console.log("logging evt:", evt);
         const cur = menuRef.current;
         const node = evt.target;
         console.log("logging cur:", cur);
