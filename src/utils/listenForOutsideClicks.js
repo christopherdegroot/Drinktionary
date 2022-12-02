26d5703701c9324bffa96d89127c4292668d5f72
Chris De Groot <christopher.neil.degroot@gmail.com>
2022-12-01 21:37:06 -0800

    remove console logs

--- src/utils/listenForOutsideClicks.js
+++ src/utils/listenForOutsideClicks.js
@@ -10,10 +10,8 @@ export default function listenForOutsideClicks(
     setListening(true);
     [`click`, `touchstart`].forEach((type) => {
       document.addEventListener(`click`, (evt) => {
-        console.log("logging evt:", evt);
         const cur = menuRef.current;
         const node = evt.target;
-        console.log("logging cur:", cur);
         if (cur.contains(node)) return;
         setIsOpen(false);
       });
